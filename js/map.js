f74acc68d5bc12d48b4c673c42ffb26e68152abf
Thomas Gallagher <thomas@keecker.com>
2016-04-25 00:24:46 +0200

    Add /polylines endpoint and javascript to draw what it returns

--- js/map.js
+++ js/map.js
@@ -1,8 +1,38 @@
+var map;
+
 function initMap() {
   // Create a map object and specify the DOM element for display.
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 48.853, lng: 2.3499},
-    scrollwheel: false,
-    zoom: 13
-  });
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 48.853, lng: 2.3499},
+        scrollwheel: false,
+        zoom: 13
+    });
+    drawAllLines();
+}
+
+function drawEncodedPolyline(encodedPolyline) {
+    var decodedPath = google.maps.geometry.encoding.decodePath(encodedPolyline);
+    var line = new google.maps.Polyline({
+        path: decodedPath,
+        geodesic: true,
+        strokeColor: '#FF0000',
+        strokeOpacity: 1.0,
+        strokeWeight: 2
+    });
+    line.setMap(map)
+}
+
+function drawAllLines() {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+            var lines = xmlHttp.responseText.split("\n")
+            var numLines = lines.length;
+            for (var i = 0; i < numLines; i++) {
+                drawEncodedPolyline(lines[i])
+            }
+        }
+    }
+    xmlHttp.open("GET", "/polylines", true); // true for asynchronous
+    xmlHttp.send(null);
 }
--- main.go
+++ main.go
@@ -4,27 +4,40 @@ import (
     "fmt"
     "net/http"
     "io/ioutil"
+    "strings"
     "google.golang.org/appengine"
     "google.golang.org/appengine/urlfetch"
     "github.com/strava/go.strava"
 )
 
+var accessToken string
+
 func init() {
-    http.HandleFunc("/activities", actHandler)
+    accessToken = strings.TrimSpace(readAccessToken())
     http.HandleFunc("/segment", segHandler)
+    http.HandleFunc("/polylines", linesHandler)
 }
 
-func actHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprint(w, "TODO")
+func linesHandler(w http.ResponseWriter, r *http.Request) {
+    client := getClient(r)
+    service := strava.NewCurrentAthleteService(client)
+    activities, err := service.ListActivities().
+        Page(1).
+        PerPage(50).
+        Do()
+    if err != nil {
+        fmt.Fprint(w, err)
+        return
+    }
+    for _,act := range activities {
+        fmt.Fprintf(w, "%s\n", act.Map.SummaryPolyline)
+    }
 }
 
 func segHandler(w http.ResponseWriter, r *http.Request) {
     var segmentId int64 = 229781
-    var accessToken string = readAccessToken()
 
-    ctx := appengine.NewContext(r)
-    urlfetch_client := urlfetch.Client(ctx)
-    client := strava.NewClient(accessToken, urlfetch_client)
+    client := getClient(r)
 
     segment, err := strava.NewSegmentsService(client).Get(segmentId).Do()
 
@@ -37,7 +50,7 @@ func segHandler(w http.ResponseWriter, r *http.Request) {
     if segment.ActivityType == strava.ActivityTypes.Run {
         verb = "run"
     }
-    fmt.Fprintf(w, "%s, %s %d times by %d athletes\n\n", segment.Name, verb, segment.EffortCount, segment.AthleteCount)   
+    fmt.Fprintf(w, "%s, %s %d times by %d athletes\n\n", segment.Name, verb, segment.EffortCount, segment.AthleteCount)
 }
 
 func readAccessToken() string {
@@ -50,4 +63,9 @@ func check(e error) {
     if e != nil {
         panic(e)
     }
+}
+
+func getClient(r *http.Request) (*strava.Client) {
+    urlFetchClient := urlfetch.Client(appengine.NewContext(r))
+    return strava.NewClient(accessToken, urlFetchClient)
 }
